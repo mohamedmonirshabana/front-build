dfeed934de4d6f1794d604e376192aa6f687952b
Mohamed Monir Shabana <mohamed.monir.shabana@gmail.com>
2023-11-13 01:40:19 +0200

    add files

---
+++ precache-manifest.e3a35aea94fba808279891ad902b52e5.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = [
+  {
+    "revision": "ceffdad354e1ac0cc537",
+    "url": "/static/css/main.527aed55.chunk.css"
+  },
+  {
+    "revision": "ceffdad354e1ac0cc537",
+    "url": "/static/js/main.ceffdad3.chunk.js"
+  },
+  {
+    "revision": "3500866aaf1a29271c30",
+    "url": "/static/css/1.a2e4f8d3.chunk.css"
+  },
+  {
+    "revision": "3500866aaf1a29271c30",
+    "url": "/static/js/1.3500866a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "08a9539dc87c49d59cdca71443cd4f08",
+    "url": "/static/media/other.08a9539d.jpeg"
+  },
+  {
+    "revision": "116fddc0ffc2c649c7e8018e77537aef",
+    "url": "/static/media/hero-bg.116fddc0.jpg"
+  },
+  {
+    "revision": "bac34c732ed04d7b993a380647f49d08",
+    "url": "/static/media/twitter.bac34c73.svg"
+  },
+  {
+    "revision": "2e2cec10c01f46c1e1b972464da1e4fa",
+    "url": "/static/media/facebook-solid.2e2cec10.svg"
+  },
+  {
+    "revision": "7bec69aaffaf2883d5d738d768106e06",
+    "url": "/static/media/instagram.7bec69aa.svg"
+  },
+  {
+    "revision": "a2dacad3fe0e2ecc91c79a32a94d1f89",
+    "url": "/static/media/skype.a2dacad3.svg"
+  },
+  {
+    "revision": "b2e987701be06374bfe9d6eab5db1748",
+    "url": "/static/media/linkedin-fill.b2e98770.svg"
+  },
+  {
+    "revision": "f5e12a2e76499ff2e179fd6a7773eb8b",
+    "url": "/static/media/student.f5e12a2e.svg"
+  },
+  {
+    "revision": "3baa8b71c647f798f6f92ffa9ca5a683",
+    "url": "/static/media/ok.3baa8b71.svg"
+  },
+  {
+    "revision": "ec8afe7986249bf8d00fb3c91043c51b",
+    "url": "/static/media/dangerous.ec8afe79.svg"
+  },
+  {
+    "revision": "8943de8bd12741a3815b2a4d0912d411",
+    "url": "/static/media/CoursD.8943de8b.jpg"
+  },
+  {
+    "revision": "3d1769781d65b7a4f63898025ffda602",
+    "url": "/static/media/save.3d176978.svg"
+  },
+  {
+    "revision": "422de31a3aff6621c3be5321dcf81755",
+    "url": "/static/media/arrow-right.422de31a.svg"
+  },
+  {
+    "revision": "8f8a5cdfb65de733b3ce8a676d5edec0",
+    "url": "/static/media/user-line.8f8a5cdf.svg"
+  },
+  {
+    "revision": "c8da1f97dfa8ba1cbe9220767b7dcca6",
+    "url": "/index.html"
+  }
+];
